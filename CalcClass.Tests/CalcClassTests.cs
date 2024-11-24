a9081b2b300f7fb8a3ee0521ad223349dac96ef1
eldarkr <goreldar@gmail.com>
2024-11-24 12:55:49 +0200

    Add project files.

---
+++ CalcClass.Tests/CalcClassTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalcClassBr;
+using System;
+
+namespace CalcClass.Tests
+{
+    [TestClass]
+    public class CalcClassTests
+    {
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", 
+            "TestList.xml", 
+            "TestWithValidData", 
+            DataAccessMethod.Sequential)]
+        public void TestValidData()
+        {
+            long a = Convert.ToInt64(TestContext.DataRow["a"]);
+            long b = Convert.ToInt64(TestContext.DataRow["b"]);
+            int expected = Convert.ToInt32(TestContext.DataRow["expected"]);
+
+            int actual = CalcClassBr.CalcClass.Add(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.XML", 
+            "TestList.xml", 
+            "TestWithInvalidData", 
+            DataAccessMethod.Sequential)]
+        public void TestInvalidData()
+        {
+            long a = Convert.ToInt64(TestContext.DataRow["a"]);
+            long b = Convert.ToInt64(TestContext.DataRow["b"]);
+
+            // Оскільки очікується виняток, результат нам не потрібен
+            CalcClassBr.CalcClass.Add(a, b);
+        }
+    }
+}
